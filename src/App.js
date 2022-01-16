55fa32f7a74acd0b43c1190fe6d2fb3c6830efe2
Benjiealcontin <Benjiealcontin@gmail.com>
2022-01-16 10:30:52 +0800

    2nd attempt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React, { Component } from "react";
-import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./auth/Login";
 import Login from "./auth/Register";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
+    <Provider store={store}>
     <BrowserRouter>
-    <div className="App">
+        <div className="App">
+      <Navbar />
+      </div>
     <Routes>
-      <Route path="/" element={<Landing />}>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Route>
+      <Route
+        path="/"
+        element={<Landing />}
+      />
+        <Route
+        path="/login"
+        element={<Login />}
+      />
+        <Route
+        path="/register"
+        element={<Register />}
+      />
     </Routes>
-    </div>
     </BrowserRouter>
+    </Provider>
   );
 }
-
 export default App;
